a635fc4091751af873ed04b1a5a5f2ee99244e9b
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2024-03-22 23:10:47 +0700

    add menu

---
+++ Assigment3/scripts/gamestate.py
@@ -0,0 +1,6 @@
+class GameState:
+  IS_EXIT = 0
+  IS_START = 1
+  OPEN_OPTS = 2
+  IS_PLAY = 3
+  IS_PAUSE = 4
--- Assigment3/scripts/utils.py
+++ Assigment3/scripts/utils.py
@@ -36,3 +36,72 @@ class Animation:
     
     def img(self):
         return self.images[int(self.frame / self.img_duration)]
+    
+def drawTextOnScreen(surface, text, font, textColor, x, y):
+
+    img = font.render(text, True, textColor)
+    surface.blit(img, (x,y))
+
+def convertGameTime2Minute(time):
+    minute = (time // FPS) // 60
+    second = (time // FPS) - 60 * minute
+    return "{:02d}:{:02d}".format(minute, second)
+
+def load_sprite_sheet(file_path, rows, cols, scale=1):
+    sheet = pygame.image.load(file_path).convert_alpha()
+    sheet_rect = sheet.get_rect()
+    frame_width = sheet_rect.width // cols
+    frame_height = sheet_rect.height // rows
+
+    scaled_frames = []
+    for row in range(rows):
+        for col in range(cols):
+            frame_rect = pygame.Rect(col * frame_width, row * frame_height, frame_width, frame_height)
+            frame = sheet.subsurface(frame_rect)
+            scaled_frame = pygame.transform.scale(frame, (int(frame_width * scale), int(frame_height * scale)))
+            scaled_frames.append(scaled_frame)
+
+    return scaled_frames
+
+def load_frames_from_sprite_sheet(file_path, size, rowIndex, numberOfCol=0, scale=1):
+    sheet = pygame.image.load(file_path).convert_alpha()
+    sheet_rect = sheet.get_rect()
+    frame_width = size[0]
+    frame_height = size[1]
+
+    scaled_frames = []
+
+    for col in range(numberOfCol):
+        frame_rect = pygame.Rect(col * frame_width, rowIndex * frame_height, frame_width, frame_height)
+        frame = sheet.subsurface(frame_rect)
+        scaled_frame = pygame.transform.scale(frame, (int(frame_width * scale), int(frame_height * scale)))
+        scaled_frames.append(scaled_frame)
+
+    return scaled_frames
+
+def fillImageColor(surface, color):
+    """Fill all pixels of the surface with color, preserve transparency."""
+    w, h = surface.get_size()
+    r, g, b, _ = color
+
+    # Create a new surface with the same size and transparency
+    newSurface = pygame.Surface((w, h), pygame.SRCALPHA)
+
+    for x in range(w):
+        for y in range(h):
+            a = surface.get_at((x, y))[3]
+            newSurface.set_at((x, y), pygame.Color(r, g, b, a))
+
+    return newSurface
+
+def getStars(points, acticeStar, unActiveStar):
+    if points == 0:
+        return 3*[unActiveStar]
+
+    if points == 1:
+        return [acticeStar] + 2*[unActiveStar]
+
+    if points == 2:
+        return 2*[acticeStar] + [unActiveStar]
+
+    return 3*[acticeStar]
